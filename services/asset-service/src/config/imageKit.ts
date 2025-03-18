25a2452d4691b52a51faffb65c2ef70e8663ae4b
pkostadinov72 <pkostadinov72@gmail.com>
2025-03-18 17:40:19 +0200

    chore: better naming for ImageKit sdk;

--- services/asset-service/src/config/imageKit.ts
+++ services/asset-service/src/config/imageKit.ts
@@ -1,7 +1,7 @@
 import ImageKit from "imagekit";
 import { config } from "./config";
 
-export const imagekit = new ImageKit({
+export const imageKit = new ImageKit({
   publicKey: config.IMAGEKIT.IMAGEKIT_PUBLIC_KEY,
   privateKey: config.IMAGEKIT.IMAGEKIT_PRIVATE_KEY,
   urlEndpoint: config.IMAGEKIT.IMAGEKIT_URL_ENDPOINT
